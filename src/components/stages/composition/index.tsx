f502555b9582a00b473745ce561d1d0bb5437726
NavasardianMichael <navasardianmichael@gmail.com>
2024-02-28 19:34:42 +0400

    changes

--- src/components/stages/composition/index.tsx
+++ src/components/stages/composition/index.tsx
@@ -1,15 +1,32 @@
 import { StageComponent } from 'helpers/types/stage'
 import styles from './styles.module.css'
 import { ALPHABET } from 'helpers/constants/app'
+import { MouseEventHandler, useState } from 'react'
 
 export const Composition: StageComponent = ({  }) => {
+
+  const [word, setWord] = useState('')
+
+  const handleAlphabetLetterClick: MouseEventHandler<HTMLButtonElement> = e => {
+    setWord(word + e.currentTarget.name)
+  }
+
   return (
     <div className={styles.composition}>
+      <div className={styles.word}>
+        {
+          Array.from(word).map((letter, i) => {
+            return (
+              <span key={i} className={styles.letter}>{letter}</span>
+            )
+          })
+        }
+      </div>
       <div className={styles.alphabet}>
         {
           ALPHABET.map(letter => {
             return (
-              <button className={styles.letter} key={letter}>
+              <button className={styles.letter} key={letter} name={letter} onClick={handleAlphabetLetterClick}>
                 {letter}
               </button>
             )
