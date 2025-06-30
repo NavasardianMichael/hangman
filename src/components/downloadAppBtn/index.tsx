d17f418bee33e94f863a08f63a4073283b98547d
NavasardianMichael <navasardianmichael@gmail.com>
2025-06-30 16:10:08 +0400

    trying to add download app custom button

---
+++ src/components/downloadAppBtn/index.tsx
@@ -0,0 +1,97 @@
+import {
+  DownloadOutlined
+} from '@ant-design/icons';
+import type { NotificationArgsProps } from 'antd';
+import { Button, notification } from 'antd';
+import { createContext, MouseEventHandler, useEffect, useMemo, useRef } from 'react';
+
+type NotificationPlacement = NotificationArgsProps['placement'];
+
+const Context = createContext({ name: 'Default' });
+
+export const DownloadAppBtn = () => {
+  const [api, contextHolder] = notification.useNotification();
+  const deferredPrompt = useRef<any | null>(null);
+  const timeout = useRef<NodeJS.Timeout | null>(null);
+
+
+  useEffect(() => {
+    const preservePrompt = (event: Event) => {
+      alert(1)
+      // Prevent the mini-infobar from appearing on mobile
+      event.preventDefault();
+      // Stash the event so it can be triggered later
+      if (!deferredPrompt) return;
+      deferredPrompt.current = event;
+    }
+
+    window.addEventListener('beforeinstallprompt', preservePrompt);
+
+    return () => {
+      // Clean up the event listener
+      window.removeEventListener('beforeinstallprompt', preservePrompt);
+    }
+  }, [])
+
+  useEffect(() => {
+    const openDownloadNotification = () => {
+      openNotification('top')
+    }
+
+    timeout.current = setTimeout(() => {
+      openDownloadNotification();
+    }, 1000);
+
+    return () => {
+      if (timeout.current) clearTimeout(timeout.current);
+    }
+
+  }, []);
+
+  const downloadAppBtnClick: MouseEventHandler<HTMLElement> = async (event) => {
+    console.log({ deferredPrompt });
+
+    if (!deferredPrompt.current) return;
+
+    // Show the install prompt
+    deferredPrompt.current.prompt();
+
+    // Wait for the user to respond to the prompt
+    const { outcome } = await deferredPrompt.current.userChoice;
+
+    if (outcome === 'accepted') {
+      console.log('User accepted the install prompt');
+    } else {
+      console.log('User dismissed the install prompt');
+    }
+
+    deferredPrompt.current = null;
+  }
+
+  const openNotification = (placement: NotificationPlacement) => {
+    api.info({
+      message: `Notification ${placement}`,
+      description: <Context.Consumer>{() => {
+        return (
+          <Button
+            type="dashed"
+            style={{ width: '100%' }}
+            onClick={downloadAppBtnClick}
+            icon={<DownloadOutlined />}
+          >
+            Ներբեռնել հավելվածը
+          </Button>
+        )
+      }}</Context.Consumer>,
+      placement,
+    });
+  };
+
+  const contextValue = useMemo(() => ({ name: 'Ant Design' }), []);
+
+  return (
+    <Context.Provider value={contextValue}>
+      {contextHolder}
+    </Context.Provider>
+  );
+};
--- src/components/stages/start/index.tsx
+++ src/components/stages/start/index.tsx
@@ -1,6 +1,8 @@
 import { StageComponent } from 'helpers/types/stage'
 import styles from './styles.module.css'
 import { CustomButton } from 'components/shared/customButton'
+import { Button } from 'antd'
+import { DownloadAppBtn } from 'components/downloadAppBtn'
 
 export const Start: StageComponent = ({ toNextPage }) => {
 
@@ -10,6 +12,7 @@ export const Start: StageComponent = ({ toNextPage }) => {
 
   return (
     <div className={styles.start}>
+      <DownloadAppBtn />
       <CustomButton onClick={handleClick}>
         սկսել
       </CustomButton>
